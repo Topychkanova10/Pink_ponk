cd4fa8f63d5b8a908b764269a96907fc15215fe5
Topychkanova10 <155504128+Topychkanova10@users.noreply.github.com>
2024-01-08 11:43:08 +0800

    Add files via upload

--- Ping_pong.py
+++ Ping_pong.py
@@ -19,22 +19,43 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x,self.rect.y))#отобразить фон картинку в окне
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()#список состояния всех клавиш клавиатуры
 
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, 20)
-        bullets.add(bullet)
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 330:#опускаемся вниз если по Y меньше 420
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()#список состояния всех клавиш клавиатуры
+
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 330:#опускаемся вниз если по Y меньше 420
+            self.rect.y += self.speed
+player_left = Player('racket.png', 35, 250, 35, 170, 5)
+player_right = Player('racket.png', 650, 250, 35, 170, 5)
+tenis_ball = GameSprite('tenis_ball.png', 370, 200, 45, 45, 5)
 
 game = True
+finish = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill((227, 1, 254))
+        player_left.update_l()
+        player_right.update_r()
+        tenis_ball.rect.y += 3
+        tenis_ball.rect.x += 3
+
+        if sprite.collide_rect(player_left, tenis_ball) or sprite.collide_rect(player_right, tenis_ball):
+            tenis_ball.rect.y *= -1 
+            tenis_ball.rect.x *= -1  
+        player_left.reset()
+        player_right.reset()
+        tenis_ball.reset()
+
     display.update()
     timer.tick(FPS)
-
